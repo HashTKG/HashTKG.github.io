835a71e9a9c539d68140439045700da7f4e8e3c7
TARUN KUMAR GUPTA <princtarun35@gmail.com>
2023-04-22 15:04:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ toggleIcon.addEventListener('click', () =>{
     toggleIcon.classList.toggle('bx-sun');
     document.body.classList.toggle('dark-mode');
 })
+
+// menu icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+//remove menu icon navbar when click navbar link (scroll)
+window.onscroll = () => {
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
